a8146b58779208227d9027d6dfb2d92973a6e9ca
Matrix <codecolor@outlook.com>
2018-04-24 15:54:52 +0800

    创建celery实例

--- proj/celery.py
+++ proj/celery.py
@@ -5,8 +5,14 @@
 # @Site: https://github.com/blackmatrix7
 # @File: celery.py
 # @Software: PyCharm
+from celery import Celery
+from django.conf import settings
 
 __author__ = 'blackmatrix'
 
-if __name__ == '__main__':
-    pass
+# 创建celery应用
+celery_app = Celery('proj', broker=settings.CELERY_BROKER_URL)
+# 从配置文件中加载除celery外的其他配置
+celery_app.config_from_object('django.conf:settings')
+# 自动检索每个app下的tasks.py
+celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
