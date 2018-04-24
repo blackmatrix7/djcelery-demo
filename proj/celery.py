86493a42badea50198596c0a96dfc05cbd83b965
Matrix <codecolor@outlook.com>
2018-04-24 15:47:06 +0800

    创建celery实例

---
+++ proj/celery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2018/4/24 下午3:46
+# @Author: BlackMatrix
+# @Site: https://github.com/blackmatrix7
+# @File: celery.py
+# @Software: PyCharm
+
+__author__ = 'blackmatrix'
+
+if __name__ == '__main__':
+    pass
