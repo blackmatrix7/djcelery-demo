200992ada32f1f87f88a92c1339dde82713c3cf6
Matrix <codecolor@outlook.com>
2018-04-24 16:18:08 +0800

    增加用于演示的异步任务

--- demo/tasks.py
+++ demo/tasks.py
@@ -5,8 +5,15 @@
 # @Site: https://github.com/blackmatrix7
 # @File: tasks.py
 # @Software: PyCharm
-
+import logging
+from proj.celery import celery_app
 __author__ = 'blackmatrix'
+
+
+@celery_app.task
+def async_task():
+    logging.info('run async_task')
+
 
 if __name__ == '__main__':
     pass
