d36aa89b09983c8f583a65da147b736697e82663
Matrix <codecolor@outlook.com>
2018-04-24 16:06:33 +0800

    增加markdown所需的图片

---
+++ demo/tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2018/4/24 下午3:58
+# @Author: BlackMatrix
+# @Site: https://github.com/blackmatrix7
+# @File: tasks.py
+# @Software: PyCharm
+
+__author__ = 'blackmatrix'
+
+if __name__ == '__main__':
+    pass
