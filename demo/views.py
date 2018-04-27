b4ee577c2f08010bc98e00e2f11461234ef1f8cd
Matrix <codecolor@outlook.com>
2018-04-27 14:21:21 +0800

    增加通过Model获取任务的示例

--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,44 @@
+import json
+import uuid
+import decimal
+import datetime
 from django.http import HttpResponse
 from demo.tasks import async_demo_task
+from djcelery.models import PeriodicTask
+from django.utils.timezone import is_aware
+from django.utils.functional import Promise
+from django.utils.duration import duration_iso_string
+from django.core.serializers.json import DjangoJSONEncoder
+
+from django.forms.models import model_to_dict
+
+
+class CustomJSONEncoder(DjangoJSONEncoder):
+
+    datetime_format = '%Y-%m-%d %H:%M:%S'
+    date_format = '%Y-%m-%d'
+
+    def default(self, o):
+        # See "Date Time String Format" in the ECMA-262 specification.
+        if isinstance(o, datetime.datetime):
+            r = o.strftime(CustomJSONEncoder.datetime_format)
+            return r
+        elif isinstance(o, datetime.date):
+            r = o.strftime(CustomJSONEncoder.date_format)
+            return r
+        elif isinstance(o, datetime.time):
+            if is_aware(o):
+                raise ValueError("JSON can't represent timezone-aware times.")
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:12]
+            return r
+        elif isinstance(o, datetime.timedelta):
+            return duration_iso_string(o)
+        elif isinstance(o, (decimal.Decimal, uuid.UUID, Promise)):
+            return str(o)
+        else:
+            return super().default(o)
 
 
 # Create your views here.
@@ -7,5 +46,15 @@ def demo_task(request):
     async_demo_task.delay()
     return HttpResponse('任务已经运行')
 
+
+def get_periodic_task_list(request):
+    """
+    获取周期性任务列表
+    :return:
+    """
+    periodic_task_list = PeriodicTask.objects.all()
+    data = [model_to_dict(periodic_task) for periodic_task in periodic_task_list]
+    resp = json.dumps(data, cls=CustomJSONEncoder, ensure_ascii=False)
+    return HttpResponse(resp, content_type='application/json', status=200)
 
 
